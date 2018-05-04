ac17bd4437c62ae54bc941c794a1c226194c0e7d
WarHatch <staskeviciuskarolis@gmail.com>
2018-05-04 22:23:54 +0300

    Dropping slots, checking for slots, printing the board

--- lib/board.rb
+++ lib/board.rb
@@ -1,16 +1,41 @@
 # 7 x 6 connect four board
 class Board
-  attr_reader :slots
+  attr_reader :slots, :COLUMNS, :ROWS
 
   def initialize
-    @COLUMNS = 6
-    @ROWS = 7
+    @COLUMNS = 7
+    @ROWS = 6
     @slots = Array.new(@COLUMNS * @ROWS)
   end
 
   def drop_in(color, column)
-    row = 0
-    row += 1 until @slots[row * 7 + column - 1].nil? || row > @ROWS
-    @slots[row * 7 + column - 1] = color
+    row = 1
+    row += 1 until check_slot(column, row).nil? || row > @ROWS
+    @slots[(row - 1) * 7 + column - 1] = color
+  end
+
+  # returns a value from slot array by coordinates 
+  def check_slot(column, row)
+    @slots[(row - 1) * @COLUMNS + column - 1]
+  end
+
+  # Used to show border of the board
+  def print_board
+    row_index = 0
+    @ROWS.times do
+      col_index = 0
+      @COLUMNS.times do
+        slot = @slots[row_index * @COLUMNS + col_index]
+        slot = '*' if slot.nil?
+        print slot
+        col_index += 1
+      end
+      print "\n"
+      row_index += 1
+    end
   end
 end
+
+subject = Board.new
+2.times { subject.drop_in(:R, 1) }
+subject.print_board
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -11,10 +11,37 @@ describe Board do
     Board.new
   end
 
-  describe 'drop_in' do
+  describe '#check_slot' do
+    context 'checking first column' do
+
+      it 'finds nothing and returns a nil' do
+        expect(subject.check_slot(1, 1)).to eql nil
+      end
+
+      context 'after a red slot has been dropped 2 times in the first column' do
+        before do
+          2.times { subject.drop_in(:R, 1) }
+        end
+        it 'finds a red on the first row' do
+          expect(subject.check_slot(1, 2)).to eql :R
+        end
+        it 'finds a red on the second row' do
+          expect(subject.check_slot(1, 2)).to eql :R
+        end
+      end
+    end
+  end
+
+  describe '#drop_in' do
     it 'drops in yellow to an empty board' do
       subject.drop_in(:Y, 1)
       expect(subject.slots[0]).to eql :Y
+    end
+
+    it 'drops in yellow on top of a previous token' do
+      subject.drop_in(:Y, 1)
+      subject.drop_in(:Y, 1)
+      expect(subject.slots[subject.COLUMNS * 1 + 0]).to eql :Y
     end
   end
 
